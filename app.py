2bfa1ca8bfbd5a1cd373549c25bdeb12216fdc4b
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-09-06 04:46:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ st.set_page_config(page_title="INVESTIGATION AND PREDICTION OF TERRORISM IN NIGE
 #    st.markdown(f"<style>{app_css.read()}</style>", unsafe_allow_html=True)
 
 
-import streamlit as st
-
 @st.cache_data
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
@@ -34,18 +32,19 @@ def get_base64_of_bin_file(bin_file):
 
 def set_image_as_page_bg(image_file):
     bin_str = get_base64_of_bin_file(image_file)
-    page_bg_img = '''
+    page_bg_img = f'''
     <style>
-    body {
-    background-image: url("data:image/jpeg;base64,%s");
-    background-size: cover;
-    }
+    .stApp {{
+        background-image: url("data:image/jpeg;base64,{bin_str}");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }}
     </style>
-    ''' % bin_str
-    
+    '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
 
+# Call this function at the start of your app to set the background image
 set_image_as_page_bg('background.JPG')
 
     
