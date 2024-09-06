c8e3c6076396d587ffc4b26f8d904fac005c4b99
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-09-06 04:37:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,23 +23,20 @@ st.set_page_config(page_title="INVESTIGATION AND PREDICTION OF TERRORISM IN NIGE
 #with open("index.css") as app_css:
 #    st.markdown(f"<style>{app_css.read()}</style>", unsafe_allow_html=True)
 
-@st.cache_data
-def get_base64_of_bin_file(bin_file):
-    try:
-        with open(bin_file, 'rb') as f:
-            data = f.read()
-        return base64.b64encode(data).decode()
-    except (FileNotFoundError, EOFError) as e:
-        st.error(f"Error reading file: {e}")
-        return None
+import base64
 
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
 def set_png_as_page_bg(png_file):
     bin_str = get_base64_of_bin_file(png_file)
     page_bg_img = '''
     <style>
     body {
-    background-image: url("data:image/png;base64,%s");
+    background-image: url("data:image/jpeg;base64,%s");
     background-size: cover;
     }
     </style>
@@ -49,6 +46,7 @@ def set_png_as_page_bg(png_file):
     return
 
 set_png_as_page_bg('background.JPG')
+    
 
 #page_bg_img = '''
 #<style>
