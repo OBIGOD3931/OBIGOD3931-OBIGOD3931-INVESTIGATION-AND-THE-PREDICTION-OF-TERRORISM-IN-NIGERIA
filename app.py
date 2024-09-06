4fa2ca86eedcb7bc47c86ba24dbcd05ef3da5c9e
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-09-06 04:41:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,16 +23,17 @@ st.set_page_config(page_title="INVESTIGATION AND PREDICTION OF TERRORISM IN NIGE
 #with open("index.css") as app_css:
 #    st.markdown(f"<style>{app_css.read()}</style>", unsafe_allow_html=True)
 
-import base64
 
-@st.cache(allow_output_mutation=True)
+import streamlit as st
+
+@st.cache_data
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
+def set_image_as_page_bg(image_file):
+    bin_str = get_base64_of_bin_file(image_file)
     page_bg_img = '''
     <style>
     body {
@@ -45,7 +46,8 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('background.JPG')
+set_image_as_page_bg('background.JPG')
+
     
 
 #page_bg_img = '''
